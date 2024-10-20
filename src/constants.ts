8dd258494f4432f00bbae39b1d57e251ea2d50c4
Eoic <karolis.strazdas@protonmail.com>
2024-10-21 00:10:24 +0300

    Re-add network manager, and .env.

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,11 @@
-// Scene.
+// Scene - World.
 export const WORLD_WIDTH = 1000;
 export const WORLD_HEIGHT = 1000;
+
+// Scene - Viewport.
 export const ZOOM = { MIN: 0.25, MAX: 5 };
+
+// Scene - Grid.
 export const CELL_SIZE = 100;
 export const CELL_LINE_WIDTH = 2;
 export const CELL_FULL_SIZE = CELL_SIZE + CELL_LINE_WIDTH;
@@ -12,6 +16,3 @@ export const CELL_COLOR = {
     SELECTION_FILL: 0xafa9f9,
     BORDER: 0xadc178,
 };
-
-// Network.
-export const WEB_SOCKET_URL = 'ws://127.0.0.1:6789';
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import '../styles/main.scss';
 import { Scene } from './scene';
+import { ConnectionManager } from './connection-manager';
+
+const connectionManager = new ConnectionManager(import.meta.env.VITE_WEBSOCKET_URL);
+
+connectionManager.on('open', () => {
+    connectionManager.send({ type: 'PING', data: 'Hello there' });
+});
+
+connectionManager.on('message', (data) => {
+    console.log('Got message:', data);
+});
 
 new Scene();
