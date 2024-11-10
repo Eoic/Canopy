795a630e819c91598d6d9b7119366fc4ea441962
Eoic <karolis.strazdas@protonmail.com>
2024-11-11 00:14:15 +0200

    Remove redundant logging.

--- src/utils/user-utils.ts
+++ src/utils/user-utils.ts
@@ -1,9 +1,14 @@
 import { cyrb53 } from './crypto';
 
-export const USER_COLORS = [0xF00699, 0x018E42, 0x454E9E, 0x52414C, 0xD14081, 0xD66853];
+export const USER_COLORS = [
+    0xDC143C,
+    0x1E555C,
+    0x454E9E,
+    0x52414C,
+    0xD10A81,
+    0xB6422B
+];
 
 export const getUserColor = (id: string) => {
-    const index = cyrb53(id) % USER_COLORS.length;
-    console.log(index);
-    return USER_COLORS[index];
+    return USER_COLORS[cyrb53(id) % USER_COLORS.length];
 };
